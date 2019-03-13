1ad7e8dce2db069a376a3a29da5f66c86d6f61e7
Arthur POIRET <dropsnorz@gmail.com>
2019-03-13 20:53:58 +0100

    initial commit

---
+++ Source/Main.cpp
@@ -0,0 +1,20 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+    It contains the basic startup code for a JUCE application.
+
+  ==============================================================================
+*/
+#include <iostream>
+#include "../JuceLibraryCode/JuceHeader.h"
+
+//==============================================================================
+int main (int argc, char* argv[])
+{
+	std::cout << "Hello, World!";
+	std::cin.ignore();
+
+    return 0;
+}
